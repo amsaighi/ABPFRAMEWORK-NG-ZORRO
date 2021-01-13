b73df932635d790d901f2af16162ab7ab30ddc54
Amin Saighi <amin.saighi@gmail.com>
2021-01-13 19:50:34 +0100

    Clean ui

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,7 +1,4 @@
-import { AuthService } from '@abp/ng.core';
 import { Component } from '@angular/core';
-import { ReplaceableComponentsService } from '@abp/ng.core'; // imported ReplaceableComponentsService
-import { OAuthService } from 'angular-oauth2-oidc';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
